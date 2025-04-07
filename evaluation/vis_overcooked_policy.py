7fb6cc8529c83c0b2e0d0b3b33c6f40f59c40c0f
Johnny Liu <jyliu@cs.utexas.edu>
2025-04-07 16:45:12 -0500

    feat: add visualizer for trained overcooked policies identical to lbf

---
+++ evaluation/vis_overcooked_policy.py
@@ -0,0 +1,116 @@
+'''
+Script to rollout a policy for a given number of episodes on the overcooked environment.
+Differs from vis_lbf_policy.py in that it loads policies from ippo rather than fcp
+TODO: add support for loading policies from fcp
+'''
+import jax
+from envs import make_env
+from evaluation.policy_loaders import MLPActorCriticLoader, S5ActorCriticLoader, RandomActor
+import jax.numpy as jnp
+
+from common.save_load_utils import load_checkpoints
+from envs.overcooked.overcooked_visualizer_v2 import OvercookedVisualizerV2
+
+def rollout(ego_run_path, partner_run_path, 
+            ego_seed_idx, partner_seed_idx,
+            ego_checkpoint_idx, partner_checkpoint_idx,
+            num_episodes, render, 
+            savevideo, save_name, 
+            kwargs, verbose
+            ):
+    env = make_env('overcooked-v2', kwargs)
+
+    policies = {}
+    policies[0] = MLPActorCriticLoader(ego_run_path, env.action_space(env.agents[0]).n, 
+                                      n=ego_seed_idx, m=ego_checkpoint_idx)
+    policies[1] = MLPActorCriticLoader(partner_run_path, env.action_space(env.agents[0]).n, 
+                                      n=partner_seed_idx, m=partner_checkpoint_idx) 
+    # Rollout
+    states = []
+    key = jax.random.PRNGKey(112358)
+
+    for episode in range(num_episodes):
+        key, subkey = jax.random.split(key)
+        obs, state = env.reset(subkey)
+
+        done = {agent: False for agent in env.agents}
+        done['__all__'] = False
+        total_rewards = {agent: 0.0 for agent in env.agents}
+        num_steps = 0
+        while not done['__all__']:
+            # Get available actions for each agent
+            avail_actions = env.get_avail_actions(state)
+            
+            # Sample actions for each agent
+            actions = {}
+            for i, agent in enumerate(env.agents):
+                # Construct observation dict with available actions
+                obs_dict = {
+                    'obs': obs[agent],
+                    'dones': jnp.array([done[agent]]),
+                    'avail_actions': avail_actions[agent]
+                }
+                # Policies tend to perform better on LBF in train mode
+                action, key = policies[i].act(obs_dict, key, test_mode=False)
+                actions[agent] = action
+            
+            key, subkey = jax.random.split(key)
+            obs, state, rewards, done, info = env.step(subkey, state, actions)
+
+            # Process observations, rewards, dones, and info as needed
+            for agent in env.agents:
+                total_rewards[agent] += rewards[agent]
+                if VERBOSE:
+                    print("action is ", actions[agent])
+                    print("obs", obs[agent], "type", type(obs[agent]))
+                    print("rewards", rewards[agent], "type", type(rewards[agent]))
+                    print("dones", done[agent], "type", type(done[agent]))
+                    print("info", info, "type", type(info))
+                    print("avail actions are ", avail_actions[agent])
+            num_steps += 1        
+            if render:         
+                # env.render(state)
+                states.append(state)
+        print(f"Episode {episode} finished. Total rewards: {total_rewards}. Num steps: {num_steps}")
+        
+    if render and savevideo:
+        print(f"\nSaving mp4 with {len(states)} frames...")
+        viz = OvercookedVisualizerV2()
+        viz.animate_mp4(states, env.agent_view_size, 
+            filename=f'results/overcooked/mp4/{kwargs["layout"]}_{save_name}.mp4', 
+            pixels_per_tile=32, fps=25)
+        print("MP4 saved successfully!")
+
+if __name__ == "__main__":
+    NUM_EPISODES = 2
+    RENDER = True
+    SAVEVIDEO = True
+    VERBOSE = False # whether or not we should print information at each step
+    ENV_KWARGS = { # specify the layout for overcooked 
+        "layout": "cramped_room",
+        "random_reset": False,
+        "max_steps": 400
+    }
+
+    ego_run_path = "results/overcooked-v2/ippo/2025-04-07_15-09-58/ippo_train_run" # ippo agent, trained for 3e6 steps
+    partner_run_path = "results/overcooked-v2/ippo/2025-04-07_15-09-58/ippo_train_run" # ippo training partner, trained for 3e6 steps
+    
+    import re
+    ego_name = re.findall("\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}", ego_run_path)[0]
+    partner_name = re.findall("\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}", partner_run_path)[0]
+
+    save_name = f"ippo={ego_name}_partner={partner_name}"
+    
+    rollout(ego_run_path=ego_run_path, 
+            partner_run_path=partner_run_path,
+            ego_seed_idx=0,
+            partner_seed_idx=0,
+            ego_checkpoint_idx=-1, # use last checkpoint
+            partner_checkpoint_idx=-1, # use last checkpoint
+            num_episodes=NUM_EPISODES, 
+            render=RENDER, 
+            savevideo=SAVEVIDEO,
+            save_name=save_name,
+            kwargs=ENV_KWARGS,
+            verbose=VERBOSE
+            )
