879c71fb831759df1f92b0422490d153b0821a6a
Caroline Wang <clw4542@cs.utexas.edu>
2025-04-18 18:02:33 -0500

    added reward shaping env state

--- envs/__init__.py
+++ envs/__init__.py
@@ -5,11 +5,12 @@ import jumanji
 from jumanji.environments.routing.lbf.generator import RandomGenerator
 
 from envs.jumanji_jaxmarl_wrapper import JumanjiToJaxMARL
+from envs.reward_shaping_jumanji_jaxmarl_wrapper import RewardShapingJumanjiToJaxMARL
 from envs.overcooked.overcooked_wrapper import OvercookedWrapper
 from envs.overcooked.augmented_layouts import augmented_layouts
 
 def make_env(env_name: str, env_kwargs: dict = {}):
-    if env_name == 'lbf':
+    if env_name in ['lbf', 'lbf-reward-shaping']:
         generator_args = {"grid_size": 7, "fov": 7, 
                           "num_agents": 2, "num_food": 3, 
                           "max_agent_level": 2, "force_coop": True}
@@ -23,8 +24,11 @@ def make_env(env_name: str, env_kwargs: dict = {}):
         env = jumanji.make('LevelBasedForaging-v0', 
                             generator=RandomGenerator(**generator_args),
                             **env_kwargs_copy)
-        env = JumanjiToJaxMARL(env)
-        
+        if env_name == 'lbf-reward-shaping':
+            env = RewardShapingJumanjiToJaxMARL(env)
+        else:
+            env = JumanjiToJaxMARL(env)
+
     elif env_name == 'overcooked-v2':
         layout = augmented_layouts[env_kwargs['layout']]
         env_kwargs_copy = copy.deepcopy(env_kwargs)
--- envs/reward_shaping_jumanji_jaxmarl_wrapper.py
+++ envs/reward_shaping_jumanji_jaxmarl_wrapper.py
@@ -6,6 +6,23 @@ from typing import Dict, Any, List
 from flax.struct import dataclass
 from functools import partial
 
+@dataclass
+class RewardShapingEnvState:
+    env_state: Any # a jumanji state
+    prev_env_state: Any # a jumanji state
+    avail_actions: jnp.ndarray
+    step: jnp.array
+
+REWARD_SHAPING_PARAMS = {
+    "DISTANCE_TO_NEAREST_FOOD_REW": 1.0, # Reward for moving closer to food (H1)
+    "DISTANCE_TO_FARTHEST_FOOD_REW": 1.0, # Reward for moving further from food (H2)
+    "SEQUENCE_REW": 1.0, # Reward for completing a sequence of actions (H3-H8)
+    "FOLLOWING_TEAMMATE_REW": 1.0, # Reward for following another agent
+    "CENTERED_FOOD_DISTANCE_REW": 1.0, # Reward for moving towards towards the food that is closest to the midpoint of the two agents
+    "PROXIMITY_TO_TEAMMATE_REW": 1.0, # Reward for proximity to teammate
+    "COLLECT_FOOD_REW": 1.0,
+}    
+
 class RewardShapingJumanjiToJaxMARL(JumanjiToJaxMARL):
     """
     A wrapper for Jumanji environments that implements reward shaping.
@@ -22,145 +39,103 @@ class RewardShapingJumanjiToJaxMARL(JumanjiToJaxMARL):
              of collecting the three items available in the environment.
     """
 
-    REWARD_SHAPING_PARAMS = {
-        "DISTANCE_TO_NEAREST_FOOD_REW": 1.0, # Reward for moving closer to food (H1)
-        "DISTANCE_TO_FARTHEST_FOOD_REW": 1.0, # Reward for moving further from food (H2)
-        "SEQUENCE_REW": 1.0, # Reward for completing a sequence of actions (H3-H8)
-        "FOLLOWING_TEAMMATE_REW": 1.0, # Reward for following another agent
-        "CENTERED_FOOD_DISTANCE_REW": 1.0, # Reward for moving towards towards the food that is closest to the midpoint of the two agents
-        "PROXIMITY_TO_TEAMMATE_REW": 1.0, # Reward for Proimity to teammate
-        "COLLECT_FOOD_REW": 1.0,
-    }    
-
-    def __init__(self, env, reward_shaping_params=None, agent_heuristics=None):
+    def __init__(self, env, reward_shaping_params=None):
         super().__init__(env)
 
         self.reward_shaping_params = (
             {
-                agent_id: dict(self.REWARD_SHAPING_PARAMS)
+                agent_id: dict(REWARD_SHAPING_PARAMS)
                 for agent_id in self.agents
             }
             if reward_shaping_params is None
             else reward_shaping_params
         )
 
-        self.agent_heuristics = (
-            agent_heuristics if agent_heuristics is not None
-            else {agent_id: None for agent_id in self.agents}
-        )
-
-    @partial(jax.jit, static_argnums=(0,))
-    def step(self, key, state: WrappedEnvState, actions, params=None):
-        obs, next_state, dense_reward_dict, dones, info = super().step(key, state, actions, params)
-
-        shaped_rewards_dict = self._extract_shaped_rewards(obs, state, next_state, actions)
-        total_reward_dict = {
-            agent: dense_reward_dict[agent] + shaped_rewards_dict[agent]
-            for agent in self.agents
-        }
-        return obs, next_state, total_reward_dict, dones, info
-
     @partial(jax.jit, static_argnums=(0,))
     def reset(self, key, params=None):
-        obs, state = super().reset(key, params)
+        env_state, timestep = self.env.reset(key)
+        obs = self._extract_observations(timestep.observation)
+        state = RewardShapingEnvState(env_state, 
+                                      env_state,
+                                      self._extract_avail_actions(timestep),
+                                      timestep.observation.step_count)
         return obs, state
+    
+    @partial(jax.jit, static_argnums=(0,))
+    def step(self, key, state: RewardShapingEnvState, actions, params=None):
+        key, key_reset = jax.random.split(key)
+        # Convert dict of actions to array
+        actions_array = self._actions_to_array(actions)
+        next_env_state, timestep = self.env.step(state.env_state, actions_array)
+        avail_actions = self._extract_avail_actions(timestep)
 
-    # def _extract_shaped_rewards(self, obs, state, actions):
-    #     """Calculate the shaped reward based on the original reward and the shaping parameters."""
+        next_state = RewardShapingEnvState(
+            env_state = next_env_state, 
+            prev_env_state = state.env_state,
+            avail_actions = avail_actions, 
+            step = timestep.observation.step_count
+        )
+        next_obs = self._extract_observations(timestep.observation)
+        reward = self._extract_rewards(timestep.reward)
+        done = self._extract_dones(timestep)
+        info  = self._extract_infos(timestep)
+        # Auto-reset environment based on termination
+        obs, state = jax.tree.map(
+            lambda x, y: jax.lax.select(done["__all__"], x, y), 
+            self.reset(key_reset), 
+            (next_obs, next_state)
+        )
 
-    #     shaped_reward = {}
+        shaped_rewards_dict = self._extract_shaped_rewards(obs, state.prev_env_state, state.env_state, actions)
+        total_reward_dict = {
+            agent: reward[agent] + shaped_rewards_dict[agent]
+            for agent in self.agents
+        }
 
-    #     for i, agent_id in enumerate(self.agents):
-    #         agent_heuristic = self.agent_heuristics.get(agent_id, None)
+        # add the original reward to the info dict
+        original_reward = jnp.array([reward[agent] for agent in self.agents])
+        # create a new info dictionary with all the keys of the original info dictionary plus the inew original_reward key
+        new_info = {**info, "original_reward": original_reward}
+        return obs, next_state, total_reward_dict, done, new_info
 
-    #         if agent_heuristic is None:
-    #             shaped_reward[agent_id] = 0
-    #             continue
-
-    #         if agent_heuristic == "H1":
-    #             distance_to_nearest_food_rew = self._calculate_distance_to_nearest_food_reward(obs[agent_id], state.env_state, agent_id, i)
-    #             shaped_reward[agent_id] = distance_to_nearest_food_rew
-
-    #         elif agent_heuristic == "H2":
-    #             sequence_rew = self._calculate_distance_to_furthest_food_reward(obs[agent_id], state.env_state, agent_id, i)
-    #             shaped_reward[agent_id] = sequence_rew
-
-    #         elif agent_heuristic == "H3":
-    #             sequence_H3 = [0, 1, 2]
-    #             H3_sequence_rew = self._calculate_sequence_reward(obs[agent_id], state.env_state, agent_id, i, sequence = sequence_H3)
-    #             shaped_reward[agent_id] = H3_sequence_rew
-
-    #         elif agent_heuristic == "H4":
-    #             sequence_H4 = [1, 0, 2]
-    #             H4_sequence_rew = self._calculate_sequence_reward(obs[agent_id], state.env_state, agent_id, i, sequence = sequence_H4)
-    #             shaped_reward[agent_id] = H4_sequence_rew
-
-    #         elif agent_heuristic == "H5":
-    #             sequence_H5 = [2, 0, 1]
-    #             H5_sequence_rew = self._calculate_sequence_reward(obs[agent_id], state.env_state, agent_id, i, sequence = sequence_H5)
-    #             shaped_reward[agent_id] = H5_sequence_rew
-
-    #         elif agent_heuristic == "H6":
-    #             sequence_H6 = [0, 2, 1]
-    #             H6_sequence_rew = self._calculate_sequence_reward(obs[agent_id], state.env_state, agent_id, i, sequence = sequence_H6)
-    #             shaped_reward[agent_id] = H6_sequence_rew
-
-    #         elif agent_heuristic == "H7":
-    #             sequence_H7 = [1, 2, 0]
-    #             H7_sequence_rew = self._calculate_sequence_reward(obs[agent_id], state.env_state, agent_id, i, sequence = sequence_H7)
-    #             shaped_reward[agent_id] = H7_sequence_rew
-
-    #         elif agent_heuristic == "H8":
-    #             sequence_H8 = [2, 1, 0]
-    #             H8_sequence_rew = self._calculate_sequence_reward(obs[agent_id], state.env_state, agent_id, i, sequence = sequence_H8)
-    #             shaped_reward[agent_id] = H8_sequence_rew
-
-    #         else:
-    #             shaped_reward[agent_id] = 0.0
-
-    #         collect_food_rew = self._calculate_collect_food_reward(obs[agent_id], state.env_state, agent_id, i)
-    #         shaped_reward[agent_id] += collect_food_rew
-
-    #     return shaped_reward
-
-    def _extract_shaped_rewards(self, obs, state: WrappedEnvState, new_state: WrappedEnvState, actions):
+    def _extract_shaped_rewards(self, obs, prev_env_state, env_state, actions):
         shaped_rewards = {}
 
         for agent_index, agent_id in enumerate(self.agents):
             agent_reward = 0.0
 
             agent_reward += self._calculate_distance_to_nearest_food_reward(
-                state, new_state, agent_id, agent_index
+                prev_env_state, env_state, agent_id, agent_index
             )
 
             agent_reward += self._calculate_distance_to_farthest_food_reward(
-                state, new_state, agent_id, agent_index
+                prev_env_state, env_state, agent_id, agent_index
             )
 
             agent_reward += self._calculate_following_teammate_reward(
-                state, new_state, agent_id, agent_index
+                prev_env_state, env_state, agent_id, agent_index
             )
 
             agent_reward += self._calculate_centered_food_reward(
-                state, new_state, agent_id, agent_index
+                prev_env_state, env_state, agent_id, agent_index
             )
 
             agent_reward += self._calculate_proximity_to_teammate_reward(
-                state, new_state, agent_id, agent_index
+                prev_env_state, env_state, agent_id, agent_index
             )
 
-            agent_reward += self._calculate_collect_food_reward(state, agent_id, agent_index, actions)
+            agent_reward += self._calculate_collect_food_reward(env_state, agent_id, agent_index, actions)
 
             shaped_rewards[agent_id] = agent_reward
 
         return shaped_rewards
 
     def _calculate_distance_to_nearest_food_reward(self, prev_state, new_state, agent_id: str, i: int):
-        old_pos = prev_state.env_state.agents.position[i]
-        new_pos = new_state.env_state.agents.position[i]
+        old_pos = prev_state.agents.position[i]
+        new_pos = new_state.agents.position[i]
 
-        food_pos = prev_state.env_state.food_items.position
-        food_eaten = prev_state.env_state.food_items.eaten
+        food_pos = prev_state.food_items.position
+        food_eaten = prev_state.food_items.eaten
         uneaten_mask = ~food_eaten
 
         masked_food_pos = jnp.where(uneaten_mask[:, None], food_pos, jnp.array([jnp.inf, jnp.inf]))
@@ -178,11 +153,11 @@ class RewardShapingJumanjiToJaxMARL(JumanjiToJaxMARL):
         return jnp.where(jnp.isfinite(old_dist), reward, 0.0)
 
     def _calculate_distance_to_farthest_food_reward(self, prev_state, new_state, agent_id: str, i: int):
-        old_pos = prev_state.env_state.agents.position[i]
-        new_pos = new_state.env_state.agents.position[i]
+        old_pos = prev_state.agents.position[i]
+        new_pos = new_state.agents.position[i]
 
-        food_pos = prev_state.env_state.food_items.position
-        food_eaten = prev_state.env_state.food_items.eaten
+        food_pos = prev_state.food_items.position
+        food_eaten = prev_state.food_items.eaten
         uneaten_mask = ~food_eaten
 
         masked_food_pos = jnp.where(uneaten_mask[:, None], food_pos, jnp.array([-jnp.inf, -jnp.inf]))
@@ -218,11 +193,11 @@ class RewardShapingJumanjiToJaxMARL(JumanjiToJaxMARL):
     #     return reward, sequence_progress
     
     def _calculate_following_teammate_reward(self, prev_state, new_state, agent_id: str, i: int):
-        old_pos = prev_state.env_state.agents.position[i]
-        new_pos = new_state.env_state.agents.position[i]
+        old_pos = prev_state.agents.position[i]
+        new_pos = new_state.agents.position[i]
 
-        old_teammate_pos = prev_state.env_state.agents.position[1 - i]
-        new_teammate_pos = new_state.env_state.agents.position[1 - i]
+        old_teammate_pos = prev_state.agents.position[1 - i]
+        new_teammate_pos = new_state.agents.position[1 - i]
 
         old_dist = jnp.sum(jnp.abs(old_pos - old_teammate_pos))
         new_dist = jnp.sum(jnp.abs(new_pos - new_teammate_pos))
@@ -232,14 +207,14 @@ class RewardShapingJumanjiToJaxMARL(JumanjiToJaxMARL):
         return jnp.where(jnp.isfinite(old_dist), reward, 0.0)
     
     def _calculate_centered_food_reward(self, prev_state, new_state, agent_id: str, i: int):
-        old_pos = prev_state.env_state.agents.position[i]
-        new_pos = new_state.env_state.agents.position[i]
+        old_pos = prev_state.agents.position[i]
+        new_pos = new_state.agents.position[i]
 
-        teammate_pos = prev_state.env_state.agents.position[1 - i]
+        teammate_pos = prev_state.agents.position[1 - i]
         midpoint = (old_pos + teammate_pos) / 2.0
 
-        food_pos = prev_state.env_state.food_items.position
-        food_eaten = prev_state.env_state.food_items.eaten
+        food_pos = prev_state.food_items.position
+        food_eaten = prev_state.food_items.eaten
         uneaten_mask = ~food_eaten
         masked_food_pos = jnp.where(uneaten_mask[:, None], food_pos, jnp.array([jnp.inf, jnp.inf]))
 
@@ -255,21 +230,21 @@ class RewardShapingJumanjiToJaxMARL(JumanjiToJaxMARL):
         return jnp.where(jnp.isfinite(old_dist), reward, 0.0)
 
     def _calculate_proximity_to_teammate_reward(self, prev_state, new_state, agent_id: str, i: int):
-        new_pos = new_state.env_state.agents.position[i]
-        new_teammate_pos = new_state.env_state.agents.position[1 - i]
+        new_pos = new_state.agents.position[i]
+        new_teammate_pos = new_state.agents.position[1 - i]
         new_dist = jnp.sum(jnp.abs(new_pos - new_teammate_pos))
         reward_val = self.reward_shaping_params[agent_id]["PROXIMITY_TO_TEAMMATE_REW"]
         reward = reward_val * (1.0 / (new_dist + 1.0))
         return reward
 
     def _calculate_collect_food_reward(self, state, agent_id: str, i: int, actions: Dict[str, int]) -> float:
-        agent_pos = state.env_state.agents.position[i]
+        agent_pos = state.agents.position[i]
         teammate_idx = 1 - i
         teammate_id = f'agent_{teammate_idx}'
-        teammate_pos = state.env_state.agents.position[teammate_idx]
+        teammate_pos = state.agents.position[teammate_idx]
 
-        food_pos = state.env_state.food_items.position
-        food_eaten = state.env_state.food_items.eaten
+        food_pos = state.food_items.position
+        food_eaten = state.food_items.eaten
         uneaten_mask = ~food_eaten
         masked_food_pos = jnp.where(uneaten_mask[:, None], food_pos, jnp.array([jnp.inf, jnp.inf]))
 
