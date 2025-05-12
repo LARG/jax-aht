9d4196998a3c5ebaab37f80d1fcd90bc19de655c
adityamkk <adityamkk@gmail.com>
2025-05-12 12:06:46 -0500

    feat: scatterplot creation added

--- visualization/dim_reductions.py
+++ visualization/dim_reductions.py
@@ -4,45 +4,41 @@
 from typing import List
 import jax
 import jax.numpy as jnp
-from jax import vmap
 
 def PCA(points: List[jax.Array]) -> List[jax.Array]:
     # take the input (list of points in N-d space) and map to a list of 2d using PCA
     # we should implement using jax because this is time sensitive?
 
-    # Number of components to reduce points into (by default 2)
+    # number of components in the final PCA (by default 2)
     n_components = 2
 
-    # Flatten each N-dimensional array
-    flat_arrays = [jnp.ravel(x) for x in points]
-    
-    # Stack into 2D data matrix: (num_samples, features)
-    X = jnp.stack(flat_arrays)
+    # Stack the list of 1D points into a (num_points, num_dims) matrix
+    X = jnp.stack(points)
 
-    # Center the data
+    # Center the data (zero mean)
     X_mean = jnp.mean(X, axis=0)
     X_centered = X - X_mean
 
-    # Compute covariance matrix (features x features)
+    # Compute covariance matrix
     cov_matrix = jnp.cov(X_centered, rowvar=False)
 
-    # Eigen decomposition (symmetric matrices => use eigh)
+    # Eigen decomposition (symmetric matrix)
     eigvals, eigvecs = jnp.linalg.eigh(cov_matrix)
 
-    # Sort eigenvectors by eigenvalues in descending order
+    # Select top n_components eigenvectors
     sorted_indices = jnp.argsort(eigvals)[::-1]
-    top_components = eigvecs[:, sorted_indices[:n_components]]
+    top_vectors = eigvecs[:, sorted_indices[:n_components]]
 
-    # Project data
-    X_pca = jnp.dot(X_centered, top_components)
+    # Project the data
+    X_pca = X_centered @ top_vectors
 
-    # Split back into list of JAX arrays
-    return [X_pca[i] for i in range(X_pca.shape[0])]    
+    # Return as list of 2D JAX arrays
+    return [X_pca[i] for i in range(X_pca.shape[0])]  
 
 # TODO: implement / use t-SNE
 # @ Aditya
 
-def PCA(points: List[jax.Array]) -> List[jax.Array]:
+def tSNE(points: List[jax.Array]) -> List[jax.Array]:
     # take the input (list of points in N-d space) and map to a list of 2d using tSNE
     # we should implement using jax because this is time sensitive?
     return None    
@@ -50,8 +46,21 @@ def PCA(points: List[jax.Array]) -> List[jax.Array]:
 # TODO: implement / use t-SNE
 # @ Aditya
 
-import matplotlib
+import numpy as np
+import matplotlib as plt
 
 def visualize(points: List[jax.Array]):
-    # generate a plot
-    return
+     # Convert to NumPy for plotting
+    points_np = np.stack([np.array(p) for p in points])
+    x, y = points_np[:, 0], points_np[:, 1]
+
+    plt.figure(figsize=(6,6))
+    plt.scatter(x, y, s=50)
+
+    plt.title("Agent Visualization")
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.grid(True)
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.show()
