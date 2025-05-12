00bdd04bc42a4e71da8d5c5011997422c2fff30a
Johnny Liu <jyliu@cs.utexas.edu>
2025-05-11 21:37:46 -0500

    feat: add skeleton for todos

---
+++ visualization/encoder_decoder.py
@@ -0,0 +1,4 @@
+# TODO: implement an encoder decoder model for trajectories
+# @ Johnny
+
+# model
---
+++ visualization/event_based_plot.py
@@ -0,0 +1,8 @@
+
+# 1. collect trajectories
+
+# 2. collect event data for each trajectory
+
+# 3. aggregate event data
+
+# 4. run them through dim_reductions and plot
