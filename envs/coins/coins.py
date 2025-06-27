36104f4c82258c37c3fe84d1fa20368b2a6555da
lingyunxiao18 <lingyun.xiao@my.utexas.edu>
2025-06-27 15:44:19 -0500

    Simple wrapper test

--- envs/coins/coins.py
+++ envs/coins/coins.py
@@ -1,4 +1,4 @@
-from jaxmarl.environments.coin_games.coin_game import CoinGame 
+from jaxmarl.environments.coin_game.coin_game import CoinGame
 
 class CoinGameWrapper(CoinGame):
     def __init__(self, num_agents=2, grid_size=7, max_steps=1000, **kwargs):
@@ -7,3 +7,12 @@ class CoinGameWrapper(CoinGame):
         self._num_agents = num_agents
         self._grid_size = grid_size
         self._max_steps = max_steps
+
+    def reset(self, key=None):
+        return super().reset(key=key)
+
+    def step(self, actions):
+        return super().step(actions)
+
+    def observation(self, state, agent_id):
+        return super().observation(state, agent_id)
